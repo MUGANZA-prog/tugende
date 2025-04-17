55c61a0cc5fc4132f867b0bf833a6feb0012c358
brunorwanda4 <brunorwanda4@gmail.com>
2025-04-17 10:36:56 +0200

    start prisma backend

--- src/components/myComponents/auth-button.tsx
+++ src/components/myComponents/auth-button.tsx
@@ -1,11 +1,13 @@
+"use client"
 import MyImage from "@/components/myComponents/myImage";
 import { Button, } from "@/components/ui/button";
+import { loginWithProvidesService } from "@/services/auth/login-action";
 import React from "react";
 
 const AuthButton = () => {
   return (
     <div className=" flex flex-col space-y-4 w-96">
-      <Button size={"lg"} variant={"default"}>
+      <Button size={"lg"} onClick={() => loginWithProvidesService("google")} variant={"default"}>
         <MyImage
           src="https://cdn-icons-png.flaticon.com/512/300/300221.png"
           role="ICON"
